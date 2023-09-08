30bdc37452e0fa86f07dbebe5b9683a7f3fd6de1
BirgitKarlhuber <birgit@karlhuber.net>
2023-09-08 11:58:53 +0200

    created functions

---
+++ Lab3dabk/R/dijkstra.R
@@ -0,0 +1,15 @@
+
+
+dijkstra <- function(graph, init_node){
+  stopifnot(is.data.frame(graph), length(graph)==3)
+  
+  for(vetex in graph){
+    # dist[v] ← INFINITY
+    # prev[v] ← UNDEFINED
+    # add v to Q
+    # dist[source] ← 0
+  }
+    
+  
+  
+}
