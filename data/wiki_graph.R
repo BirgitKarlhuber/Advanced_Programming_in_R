8e72fc86ffb05d58e20c2a599312b4c4ebd023d5
BirgitKarlhuber <birgit@karlhuber.net>
2023-09-13 19:31:45 +0200

    document dataset wiki_graph

---
+++ data/wiki_graph.R
@@ -0,0 +1,14 @@
+#' Example of an unweighted graph
+#'
+#' A 'data.frame' with three variables (v1, v2 and w) that contains the edges of the graph (from 
+#' v1 to v2) with the weight of the edge (w) which represents the graph. 
+#'
+#' @format ## `wiki_graph`
+#' A data frame with 18 rows and 3 columns:
+#' \describe{
+#'   \item{v1}{(from) edge}
+#'   \item{v2}{(to) edge}
+#'   \item{w}{weight of the edge}
+#' }
+#' @source <https://en.wikipedia.org/wiki/Dijkstra's_algorithm>
+"wiki_graph"
